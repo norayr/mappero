7ec497da1d304ec03b494ba5e403b3bc3c0bc26f
Alberto Mardegan <mardy@users.sourceforge.net>
2010-02-22 21:38:24 +0200

    Add cast to suppress a compilation warning.

--- src/path.c
+++ src/path.c
@@ -1195,7 +1195,7 @@ calculate_route_cb(MapRouter *router, Path *path, const GError *error,
         return;
     }
 
-    g_object_remove_weak_pointer(G_OBJECT(dialog), p_dialog);
+    g_object_remove_weak_pointer(G_OBJECT(dialog), (gpointer)p_dialog);
 
     hildon_gtk_window_set_progress_indicator(GTK_WINDOW(dialog), FALSE);
     rdi = g_object_get_data(G_OBJECT(dialog), ROUTE_DOWNLOAD_INFO);
