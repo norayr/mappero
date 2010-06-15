0f16a756937ec471fcaef87537239373ff872e1c
Alberto Mardegan <mardy@users.sourceforge.net>
2010-06-15 22:30:47 +0300

    Add support for KML files (for path parsing only)

---
+++ mappero/kml.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2006, 2007 John Costigan.
+ * Copyright (C) 2010 Alberto Mardegan <mardy@users.sourceforge.net>
+ *
+ * This file is part of libMappero.
+ *
+ * libMappero is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libMappero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libMappero.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAP_KML_H
+#define MAP_KML_H
+
+#include <mappero/path.h>
+
+#include <gio/gio.h>
+
+gboolean map_kml_path_parse(GInputStream *stream, MapPath *path);
+
+#endif /* MAP_KML_H */
