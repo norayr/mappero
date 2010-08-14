e4cb026a96a54914c48c55513dde9d54c0af2359
Alberto Mardegan <mardy@users.sourceforge.net>
2010-08-14 11:48:40 +0300

    Start development of Qt version
    Add the needed packages to configure.ac and create a src/qt/ directory with a
    hello world-like Qt application.

---
+++ src/qt/main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QGLWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QGraphicsScene scene;
+
+    QGraphicsView view(&scene);
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setAttribute(Qt::WA_Maemo5AutoOrientation, true);
+    view.setAttribute(Qt::WA_Maemo5NonComposited, true);
+    view.setRenderHints(QPainter::Antialiasing);
+    view.setViewport(new QGLWidget);
+    view.show();
+
+    return app.exec();
+}
