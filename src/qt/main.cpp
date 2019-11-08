e787c103811aa3465c285d6645c8bcac33116b68
Alberto Mardegan <mardy@users.sourceforge.net>
2019-11-08 18:34:22 +0300

    main: Reduce the number of dependencies required in the static build

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -107,14 +107,16 @@ int main(int argc, char *argv[])
 #ifdef STATIC_BUILD
     #include <QQmlExtensionPlugin>
     Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+    Q_IMPORT_PLUGIN(QtQuick2Plugin)
+    Q_IMPORT_PLUGIN(QtQuick2WindowPlugin)
+#ifndef GEOTAGGING_ENABLED
     Q_IMPORT_PLUGIN(QmlFolderListModelPlugin)
     Q_IMPORT_PLUGIN(QmlSettingsPlugin)
-    Q_IMPORT_PLUGIN(QtQuick2Plugin)
     Q_IMPORT_PLUGIN(QtQuick2DialogsPlugin)
     Q_IMPORT_PLUGIN(QtQuick2DialogsPrivatePlugin)
-    Q_IMPORT_PLUGIN(QtQuick2WindowPlugin)
     Q_IMPORT_PLUGIN(QtQuickControls1Plugin)
     Q_IMPORT_PLUGIN(QtQuickLayoutsPlugin)
+#endif
     // Image formsts
     Q_IMPORT_PLUGIN(QJpegPlugin)
     Q_IMPORT_PLUGIN(QSvgPlugin)
